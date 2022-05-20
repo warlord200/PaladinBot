bd1b0d98ce75f8701b7f510882ad358ba9195f96
warlord200 <thedarkpaladin05@gmail.com>
2022-05-20 20:24:42 +0800

    postinstall

--- commands/saucenao.js
+++ commands/saucenao.js
@@ -36,7 +36,7 @@ module.exports = {
                 if (typeof response[i].authorName === 'string' || response[i].authorName instanceof String){
                     embed.setFooter({ text: `Author: ${response[i].authorName}`})
                 }
-                console.log(response.source[i])
+                console.log(response[i].source);
 
 
                 message.channel.send({ embeds:[embed]})
