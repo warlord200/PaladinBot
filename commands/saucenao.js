8d89e71f93585bdaa74b34a164e07cb749b830c8
warlord200 <thedarkpaladin05@gmail.com>
2022-05-22 17:12:31 +0800

    Added gif

--- commands/saucenao.js
+++ commands/saucenao.js
@@ -21,6 +21,9 @@ module.exports = {
             { name: 'Similarity', value: `Reference image is ${response[0].similarity}% similar to the original image `}
         )
         embed.setThumbnail(response[0].thumbnail);
+
+        // check if authorname is string 
+
         if (typeof response[0].authorName === 'string' || response[0].authorName instanceof String){
             embed.setFooter({ text: `Author: ${response[0].authorName}`})
         }
@@ -28,32 +31,32 @@ module.exports = {
         // check for twitter or pixiv link 
 
         if(response[0].similarity > 70){
-            embed.setTitle('Good Results found!')
-            embed.setColor('#33AB5F')
+            embed.setTitle('Sir, this is your requested sauce.');
+            embed.setColor('#33AB5F');
+            embed.setImage
+            ('https://images-ext-1.discordapp.net/external/EKDKQAsSwImLlUTKDvugutKCH1qcpbYMfzofsKmI2io/https/1.bp.blogspot.com/-iMhL9T7K8N0/XbujvmbiKDI/AAAAAAABr14/9F9TNWb3j5wMe9f4mjnFaVgcW9Ses6rfwCKgBGAsYHg/s1600/Azur%252BLane%252B-%252BEpisode%252B5%252B-%252BBelfast%252BElegant%252BCurtsie.gif');
             
             if(twitterURL.test(response[0].source)){
                 embed.addFields(
                     { name:'Link', value: response[0].source}
                 )
-
+            
             }else if(pixivURL.test(response[0].url)){
                 embed.addFields(
                     { name: 'Link', value: response[0].url}
                 )
-            }
-            for(let i = 0;i < 5;i++){
-                if(twitterURL.test(response[0].source)||pixivURL.test(response[0].url)){continue}
-                if(response[i].similarity > 70){
-                    embed.addFields(
-                        { name: `Additional Results ${i+1}:`, value: response[i].url}
-                    )
-                }else{continue}
+            
+            }else{
+                embed.addFields(
+                    { name: 'Link', value: response[0].url}
+                )
             }
 
 
         }else{
-            embed.setTitle('No Good Results found \:\(');
+            embed.setTitle(`I'm so sorry sir, can't find your sauce, this is the nearest one D:`);
             embed.setColor('#B8293D');
+            embed.setImage('https://giffiles.alphacoders.com/938/93869.gif')
             embed.addFields(
                 { name: 'Link', value: response[0].url}
             )
@@ -66,30 +69,17 @@ module.exports = {
 
 
 
-            // const embed = new MessageEmbed()
-            //         .addFields(
-            //             { name: 'Similarity', value: `Reference image is ${response[i].similarity}% similar to the original image `},
-            //             { name: 'Link', value: response[i].url}
-            //         )
-            //         .addFields(
-            //             { name: 'Site', value: `${response[i].site}`, inline: true},
-            //             { name: 'Source', value: `${response[i].source}`, inline: true}
-            //         )
-            //         .setThumbnail(response[i].thumbnail);
-            //     if(response[i].similarity > 70){
-            //         embed.setTitle(`Results ${i+1}: Good match`)
-            //         embed.setColor('#33AB5F')
-            //     }else{
-            //         embed.setTitle(`Results ${i+1}: Bad match`)
-            //         embed.setColor('#B8293D')
-            //     }
+            
 
-                // if (typeof response[i].authorName === 'string' || response[i].authorName instanceof String){
-                //     embed.setFooter({ text: `Author: ${response[i].authorName}`})
-            //     }
-
-
-            //     message.channel.send({ embeds:[embed]})
+        // for(let i = 0;i < 5;i++){
+        //     if(twitterURL.test(response[0].source)||pixivURL.test(response[0].url)){continue}
+        //     if(response[i].similarity > 70){
+        //         embed.addFields(
+        //             { name: `Additional Results ${i+1}:`, value: response[i].url}
+        //         )
+        //     }else{continue}
+        // }
+        
 
             
             
