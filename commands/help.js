d308102dd58e707157911d5d6f39e743af9c49f8
warlord200 <thedarkpaladin05@gmail.com>
2022-06-25 16:59:03 +0800

    Reworked Help

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,28 @@
+const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "help",
   description: "Help command",
   execute(message, args) {
-    message.channel.send(`
-*Thy requesteth is mine own order.*
-        
-    --help        - literally the title
-    --sauce / s   - reply to image with command to return sauce
-    --ping        - reply with pong
-    --jp / j   - translate image to text (currently only japanese)
-        `);
+    const embed = new MessageEmbed();
+    embed.setColor("#F0F8FF");
+    embed.setFooter({
+      text: "Jekyll",
+      icon_url:
+        "https://pbs.twimg.com/media/FSK1woJagAIZNbC?format=jpg&name=4096x4096",
+    });
+    embed.addFields({
+      name: "*Thy requesteth is mine own order.*",
+      value: `
+ 
+ 
+**--help**        - literally the title
+**--sauce / s**   - reply to image with command to return sauce
+**--ping**        - replies with pong!
+**--jp / j**      - translate image to text (currently only japanese)
+
+More features coming soon (hopefully)
+`,
+    });
+    message.channel.send({ embeds: [embed] });
   },
 };
