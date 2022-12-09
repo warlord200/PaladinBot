1e95e129f3fc7f934070efea3768eb2f05906b3b
warlord200 <thedarkpaladin05@gmail.com>
2022-12-09 23:12:29 +0800

    Changed discord.js ver

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ client.once(Events.ClientReady, (c) => {
 
 // Log in to Discord with your client's token
 client.login(token);
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json");
+
+// Create a new client instance
+const lient = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
